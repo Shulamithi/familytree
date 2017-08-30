fcd25e0b67eb27b15cec44eb476e5f87b0afa1f2
Mitch <mcewenmitc@ecs.vuw.ac.nz>
2017-08-30 18:17:44 +1200

    Member class extends Person

--- src/domain/Member.java
+++ src/domain/Member.java
@@ -1,3 +1,13 @@
 package domain;
-public class Member {
+
+import java.sql.Date;
+
+public class Member extends Person {
+
+	public Member(String firstName, String fatherSurname, String motherSurname, Date dateOfBirth, Date dateOfDeath,
+			byte[] photo) {
+		super(firstName, fatherSurname, motherSurname, dateOfBirth, dateOfDeath, photo);
+		// TODO Auto-generated constructor stub
+	}
 }
+
